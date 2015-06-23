57f3677c8b7d361b173055091b2e31b870a33af8
ramyb <rami@dudamobile.com>
2015-06-23 04:34:14 +0300

    ember-filepicker unit tests

---
+++ tests/unit/components/ember-filepicker-test.js
@@ -0,0 +1,50 @@
+import {
+    moduleForComponent,
+    test
+}
+from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForComponent('ember-filepicker', {
+    // specify the other units that are required for this test
+    needs: ['service:filepicker']
+});
+
+test('it renders', function(assert) {
+    assert.expect(2);
+
+    // creates the component instance
+    var component = this.subject();
+    assert.equal(component._state, 'preRender');
+
+    // renders the component to the page
+    this.render();
+    assert.equal(component._state, 'inDOM');
+});
+
+test('it opens filepicker on component element insertion', function(assert) {
+    var component = this.subject();
+
+    return new Ember.RSVP.Promise(function(resolve, reject){
+        var subscriber,
+            interval = 100,
+            counter = -1 * interval,
+            timeout = 10000;
+
+        subscriber = window.setInterval(function(){
+            counter += interval;
+            if (timeout > counter) {
+                if ($('#filepicker_dialog').length === 1) {
+                    resolve();
+                    window.clearInterval(subscriber);
+                }
+            } else {
+                reject();
+            }
+        }, interval);
+    }).then(function(){
+        assert.ok(true);
+    }).catch(function(){
+        assert.ok(false, 'Filepicker dialog did not load on component element insertion');
+    });
+});
